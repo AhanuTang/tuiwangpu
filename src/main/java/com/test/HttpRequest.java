6b5388d27f5cb9d208288fd5318b74754f51785e
lanrongshuang <lanrongshuang@100msh.com>
2018-08-15 19:23:27 +0800

    update

--- src/main/java/com/test/HttpRequest.java
+++ src/main/java/com/test/HttpRequest.java
@@ -2,146 +2,122 @@ package com.test;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-
-import javax.net.ssl.SSLContext;
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.List;
 import java.util.Map;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLContextBuilder;
-import org.apache.http.conn.ssl.TrustStrategy;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;  
+import org.apache.commons.httpclient.Header;  
+import org.apache.commons.httpclient.HttpClient;  
+import org.apache.commons.httpclient.HttpException;  
+import org.apache.commons.httpclient.HttpMethod;  
+import org.apache.commons.httpclient.HttpStatus;  
+import org.apache.commons.httpclient.NameValuePair;  
+import org.apache.commons.httpclient.methods.GetMethod;  
+import org.apache.commons.httpclient.methods.PostMethod;  
+import org.apache.commons.httpclient.params.HttpMethodParams;  
+import org.apache.http.client.methods.HttpPost;  
 
-import com.alibaba.fastjson.JSONObject;
 public class HttpRequest {
-	//请求配置，设置链接超时和读取超时
-		private static final RequestConfig config = RequestConfig.custom().setConnectTimeout(30000).setSocketTimeout(30000).build();
-		//https策略，绕过安全检查
-		private static CloseableHttpClient getSingleSSLConnection()
-				throws Exception {
-			CloseableHttpClient httpClient = null;
-			try {
-				SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
-					public boolean isTrusted(X509Certificate[] paramArrayOfX509Certificate, String paramString) throws CertificateException {
-						return true;
-					}
-				}).build();
-				SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-				//return HttpClients.custom().setDefaultRequestConfig(config).build();
-				//2017 12 14修改，忘了绕过安全检查设置了，哈哈
-				return HttpClients.custom().setSSLSocketFactory(sslsf).setDefaultRequestConfig(config).build();
-			} catch (Exception e) {
-				throw e;
-			}
-	 
-		}
-		 public static String sendGet(String url) {
-		        String result = "";
-		        BufferedReader in = null;
-		        try {
-		            URL realUrl = new URL(url);
-		            // 打开和URL之间的连接
-		            URLConnection connection = realUrl.openConnection();		          		            
-		         // 设置通用的请求属性
-					connection.setRequestProperty("Host", "www.tuiwangpu.com");
-					connection.setRequestProperty("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
-					connection.setRequestProperty("connection", "Keep-Alive");
-					connection.setRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64)");
-					connection.setConnectTimeout(5000);
-					connection.setReadTimeout(5000);
-					String cookie="UM_distinctid=1652ca5f7af28d-02b6bcb3fdc791-36465d60-100200-1652ca5f7b0524; dntsid=7fa300270e8f5878; tqName=lan%u84DD%u6708%u4EAE; DNTSessionUID=66B37F906A11D694C6F3ED0567BC5825A97928F21CEFD60E8041496E0887999A3D0776024696E4317DA6CB12C7C981FD695E38C10B79E11F18B54615F7CB2DB61CBA9E5BF2F2A460B7BB1889E6AD06ADE142B9772A601F84AC5AE55B1A5CEF3E470E70644C40DB1458992CF6459D2AC35641F009DF208E9DC5C3E9F5D4235047026AF1E569CAE383AB2F27E35144B2A1; CNZZDATA1259398474=780023260-1534048401-%7C1534169270";
-					connection.setRequestProperty("Cookie", cookie);
-		            // 建立实际的连接
-		            connection.connect();
-		            // 获取所有响应头字段
-//		            Map<String, List<String>> map = connection.getHeaderFields();
-//		            // 遍历所有的响应头字段
-//		            for (String key : map.keySet()) {
-//		                System.out.println(key + "--->" + map.get(key));
-//		            }
-		            // 定义 BufferedReader输入流来读取URL的响应
-		            in = new BufferedReader(new InputStreamReader(
-		                    connection.getInputStream()));
-		            String line;
-		            while ((line = in.readLine()) != null) {
-		                result += line;
-		            }
-		        } catch (Exception e) {
-		            System.out.println("发送GET请求出现异常！" + e);
-		            e.printStackTrace();
-		        }
-		        // 使用finally块来关闭输入流
-		        finally {
-		            try {
-		                if (in != null) {
-		                    in.close();
-		                }
-		            } catch (Exception e2) {
-		                e2.printStackTrace();
-		            }
-		        }
-		        return result;
-		    }
-
 	
-	/**
-     * 发送get请求
-     * @param url（get请求的url）
-     * @return
-	 * @throws Exception 
-     */
-    public static String doGetStr(String accessTokenUrl) throws Exception {
-		CloseableHttpClient httpClient = getSingleSSLConnection();
-        HttpGet httpGet = new HttpGet(accessTokenUrl);
-        JSONObject jsonObject = null;
-        String result = null;
-        try {
-            HttpResponse response = httpClient.execute(httpGet);
-            HttpEntity entity = response.getEntity();
-            if (entity != null) {
-                 result = EntityUtils.toString(entity, "UTF-8");
-            }
-            httpGet.releaseConnection();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
-     /**
-     * 发送post请求
-     * @param url
-     * @param param
-     * @return
-     * @throws Exception 
-     */
-    public static String doPostStr(String url,String param) throws Exception{
-		CloseableHttpClient httpClient = getSingleSSLConnection();
-        HttpPost httpPost = new HttpPost(url);
-        JSONObject jsonObject = null;
-        String result = null;
-        try {
-            httpPost.setEntity(new StringEntity(param, "UTF-8"));
-            HttpResponse response = httpClient.execute(httpPost);
-             result = EntityUtils.toString(response.getEntity(),"UTF-8");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
+	public static String sendGet(String url, String cookie) {
+		String result = "";
+		BufferedReader in = null;
+		try {
+			URL realUrl = new URL(url);
+			// 打开和URL之间的连接
+			URLConnection connection = realUrl.openConnection();
+			// 设置通用的请求属性
+			connection.setRequestProperty("Host", "www.tuiwangpu.com");
+			connection.setRequestProperty("accept",
+					"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+			connection.setRequestProperty("connection", "Keep-Alive");
+			connection.setRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64)");
+			connection.setConnectTimeout(5000);
+			connection.setReadTimeout(5000);
+			connection.setRequestProperty("Cookie", cookie);
+			// 建立实际的连接
+			connection.connect();
+			// 定义 BufferedReader输入流来读取URL的响应
+			in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += line;
+			}
+		} catch (Exception e) {
+			System.out.println("发送GET请求出现异常！" + e);
+			e.printStackTrace();
+		}
+		// 使用finally块来关闭输入流
+		finally {
+			try {
+				if (in != null) {
+					in.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	  /** 
+     * 执行一个HTTP POST请求，返回请求响应的HTML 
+     *  
+     * @param url  请求的URL地址 
+     * @param params  请求的查询参数,可以为null 
+     * @param charset 字符集 
+     * @param pretty 是否美化 
+     * @return 返回请求响应的HTML 
+     */  
+    public static String sendPost(String url, Map<String, Object> _params) { 
+    	String charset =  "UTF-8";
+    	 boolean pretty =  true;
+        StringBuffer response = new StringBuffer();  
+        HttpClient client = new HttpClient();  
+        PostMethod method = new PostMethod(url);  
+          
+        // 设置Http Post数据  
+        if (_params != null) {  
+            for (Map.Entry<String, Object> entry : _params.entrySet()) {  
+                method.setParameter(entry.getKey(), String.valueOf(entry.getValue()));  
+            }  
+        }  
+          
+        // 设置Http Post数据  方法二  
+//        if(_params != null) {  
+//            NameValuePair[] pairs = new NameValuePair[_params.size()];//纯参数了，键值对  
+//            int i = 0;  
+//            for (Map.Entry<String, Object> entry : _params.entrySet()) {  
+//                pairs[i] = new NameValuePair(entry.getKey(), String.valueOf(entry.getValue()));  
+//                i++;  
+//            }  
+//            method.addParameters(pairs);  
+//        }  
+          
+        try {  
+            client.executeMethod(method);  
+            if (method.getStatusCode() == HttpStatus.SC_OK) {  
+                // 读取为 InputStream，在网页内容数据量大时候推荐使用  
+                BufferedReader reader = new BufferedReader(new InputStreamReader(method.getResponseBodyAsStream(),  
+                        charset));  
+                String line;  
+                while ((line = reader.readLine()) != null) {  
+                    if (pretty)  
+                        response.append(line).append(System.getProperty("line.separator"));  
+                    else  
+                        response.append(line);  
+                }  
+                reader.close();  
+            }  
+        } catch (IOException e) {  
+            System.out.println("执行HTTP Post请求" + url + "时，发生异常！");  
+            e.printStackTrace();  
+        } finally {  
+            method.releaseConnection();  
+        }  
+        System.out.println("--------------------" + response.toString());  
+        return response.toString();  
+    }  
 }
--- src/main/java/com/test/SpringController.java
+++ src/main/java/com/test/SpringController.java
@@ -3,6 +3,10 @@ package com.test;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -22,53 +26,48 @@ public class SpringController {
 
 	@RequestMapping("/sendGet")
 	public String sendGet(HttpServletRequest request) throws Exception {
+		String cookie = "UM_distinctid=1652ca5f7af28d-02b6bcb3fdc791-36465d60-100200-1652ca5f7b0524; dntsid=7fa300270e8f5878; tqName=lan%u84DD%u6708%u4EAE; DNTSessionUID=66B37F906A11D694C6F3ED0567BC5825A97928F21CEFD60E8041496E0887999A3D0776024696E4317DA6CB12C7C981FD695E38C10B79E11F18B54615F7CB2DB61CBA9E5BF2F2A460B7BB1889E6AD06ADE142B9772A601F84AC5AE55B1A5CEF3E470E70644C40DB1458992CF6459D2AC35641F009DF208E9DC5C3E9F5D4235047026AF1E569CAE383AB2F27E35144B2A1; CNZZDATA1259398474=780023260-1534048401-%7C1534169270";
+
 		// 发送 GET 请求
-		String s = HttpRequest.sendGet("http://www.tuiwangpu.com/ucenterTry/hall/tbtask/");
+		String s = HttpRequest.sendGet("http://www.tuiwangpu.com/ucenterTry/hall/tbtask/", cookie);
 		System.out.println(s);
-		JSONObject json = parseHtml(s);
+		List<Order> orderlist = parseHtml(s);
 		return saveHtml(request, s);
 	}
 
-	@RequestMapping("/sendPost")
-	public String sendPost(HttpServletRequest request) throws Exception {
-		// 发送 POST 请求
-		String sr = HttpRequest.doPostStr("http://www.tuiwangpu.com/Login/DoLogin",
-				"username='lan蓝月亮'&pwd='lz487162010'");
-		System.out.println(sr);
-		return saveHtml(request, sr);
+	@RequestMapping("/sendTask")
+	public String sendTask(HttpServletRequest request) throws Exception {
+		String cookie = "UM_distinctid=1652ca5f7af28d-02b6bcb3fdc791-36465d60-100200-1652ca5f7b0524; dntsid=7fa300270e8f5878; tqName=lan%u84DD%u6708%u4EAE; DNTSessionUID=66B37F906A11D694C6F3ED0567BC5825A97928F21CEFD60E8041496E0887999A3D0776024696E4317DA6CB12C7C981FD695E38C10B79E11F18B54615F7CB2DB61CBA9E5BF2F2A460B7BB1889E6AD06ADE142B9772A601F84AC5AE55B1A5CEF3E470E70644C40DB1458992CF6459D2AC35641F009DF208E9DC5C3E9F5D4235047026AF1E569CAE383AB2F27E35144B2A1; CNZZDATA1259398474=780023260-1534048401-%7C1534169270";
+		String url = "http://www.tuiwangpu.com/Ajax/Task.ashx";
+		Map<String, Object> _params = new HashMap<String, Object>();
+		_params.put("action", "checkRobTask");
+		_params.put("itemId", 25);
+		// 发送 GET 请求
+		String s = HttpRequest.sendPost(url, _params);
+		System.out.println(s);
+		List<Order> orderlist = parseHtml(s);
+		return saveHtml(request, s);
 	}
 
-	private JSONObject parseHtml(String s) throws IOException {
-		String html = s;
-		File input = new File("D:/test.html");
-		Document doc = Jsoup.parse(input, "UTF-8", "http://www.tuiwangpu.com/");
-		// Document doc = Jsoup.parse(html);
-		Elements rows = doc.select("div[class=task-list]").get(0).select("div[class=clearfix]");
-		for (Element element : rows) {
-			String e = element.getElementsByClass("tk3").select("b").toString();
-			System.out.println(e);
-		}
-		return null;
-	}
-
-	public static void main(String[] args) throws IOException {
-		File input = new File("D:\\1534170550256.html");
-		Document doc = Jsoup.parse(input, "UTF-8", "http://www.tuiwangpu.com/");
-		// Document doc = Jsoup.parse(html);
+	private List<Order> parseHtml(String html) throws IOException {
+		Document doc = Jsoup.parse(html);
 		System.out.println("----------");
 		Elements rows = doc.select(".task-list").get(0).select(".clearfix");
+		List<Order> orderlist = new ArrayList<Order>();
 		for (Element element : rows) {
 			String task_title = element.getElementsByClass("desc").select("span").first().text();
 			String allMoney = element.getElementsByClass("tk3").select("b").first().text();
 			String giveMoney = element.getElementsByClass("tk5").select("b").first().text();
-			String taskId = element.getElementsByClass("tk7").select("a").attr("onclick"); 
-			Order order = new Order( task_title,  allMoney,  giveMoney,  taskId);
+			String taskId = element.getElementsByClass("tk7").select("a").attr("onclick").substring(19, 26);
+			Order order = new Order(task_title, allMoney, giveMoney, taskId);
+			orderlist.add(order);
 			System.out.println("----------");
 			System.out.println(task_title);
 			System.out.println(allMoney);
 			System.out.println(giveMoney);
 			System.out.println(taskId);
 		}
+		return orderlist;
 	}
 
 	private String saveHtml(HttpServletRequest request, String s) {
